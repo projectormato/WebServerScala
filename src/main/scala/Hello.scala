a1991c4047b8cd5f6579876196462668c805cc16
projectormato <k.keita811.anuy@gmail.com>
2019-05-13 01:02:06 +0900

    add for sample.

--- src/main/scala/Hello.scala
+++ src/main/scala/Hello.scala
@@ -1,10 +1,18 @@
 object Hello extends App {
-  val ans = (1 to 100).foreach {
-    case n if n % 15 == 0 => println("FizzBuzz")
-    case n if n % 3 == 0 => println("Fizz")
-    case n if n % 5 == 0 => println("Buzz")
-    case i => println(i)
-  }
+//  val ans = (1 to 100).foreach {
+//    case n if n % 15 == 0 => println("FizzBuzz")
+//    case n if n % 3 == 0 => println("Fizz")
+//    case n if n % 5 == 0 => println("Buzz")
+//    case i => println(i)
+//  }
 //  println(ans)
   println("Hello!")
+  for (i <- 0 until 10) {
+    println(i)
+  }
+  var j = 0;
+  while (j < 10) {
+    println(j)
+    j += 1;
+  }
 }
